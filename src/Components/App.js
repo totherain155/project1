30f3bc8fc1a4826c65dc5f5b80251f474fc12df6
totherain155 <totherain155@gmail.com>
2021-09-01 22:06:34 +0900

    setting route pages

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Router from "Components/Router";
+
+function App() {
+  return (
+    <>
+      <Router />
+      <h1>start</h1>
+    </>
+  );
+}
+
+export default App;
