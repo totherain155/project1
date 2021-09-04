07754e2b65e082b10c93e8ac6d2310d8c178a7c6
totherain155 <totherain155@gmail.com>
2021-09-04 22:30:05 +0900

    4.Sep

--- src/Components/Router.js
+++ src/Components/Router.js
@@ -1,13 +1,15 @@
 import React from "react";
-import { HashRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "Routes/Home";
 import Search from "Routes/Search";
 
 export default () => (
   <>
-    <Router>
-      <Route path="/" exact component={Home} />
-      <Route path="/search" exact component={Search} />
-    </Router>
+    <Switch>
+      <Router>
+        <Route path="/" exact component={Home} />
+        <Route path="/search" exact component={Search} />
+      </Router>
+    </Switch>
   </>
 );
