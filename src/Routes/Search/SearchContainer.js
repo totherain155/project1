e5987e68abb96f7f5ca20851035dc45ae174ed47
totherain155 <totherain155@gmail.com>
2021-10-03 21:02:14 +0900

    searchContainer by react hooks

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -6,32 +6,50 @@ import { tvApi } from "api";
 const SearchContainer = () => {
   const [movieResults, setMovieResults] = useState(null);
   const [tvResults, setTvResults] = useState(null);
-  const [searchTerm, setSearchTerm] = useState("code");
+  const [searchTerm, setSearchTerm] = useState("cat");
   const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleSubmit = () => {
     if (searchTerm !== "") {
       searchByTerm();
     }
   };
+  //test용 코드
+
   useEffect(() => {
     handleSubmit();
   });
 
   const searchByTerm = async () => {
+    setLoading(true);
     try {
-      const movieResults = await moviesApi.search(searchTerm);
-      const showResults = await tvApi.search(searchTerm);
-      console.log(movieResults, showResults);
-      setLoading(true);
+      const {
+        data: { results: movieResults },
+      } = await moviesApi.search(searchTerm);
+      const {
+        data: { results: tvResults },
+      } = await tvApi.search(searchTerm);
+      console.log(movieResults, tvResults);
+      setMovieResults(movieResults);
+      setTvResults(tvResults);
     } catch {
-      setLoading("Can't find results");
+      setError("Can't find results");
     } finally {
       setLoading(false);
     }
   };
 
-  return <SearchPresenter />;
+  return (
+    <SearchPresenter
+      movieResults={movieResults}
+      tvResults={tvResults}
+      searchTerm={searchTerm}
+      loading={loading}
+      error={error}
+      handleSubmit={handleSubmit}
+    />
+  );
 };
 
 export default SearchContainer;
