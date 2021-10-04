e594cd75d1124efeb558a82229610aebccafb71c
totherain155 <totherain155@gmail.com>
2021-10-04 16:15:36 +0900

    rebuilding DetailContainer

--- src/Routes/Detail/DetailContainer.js
+++ src/Routes/Detail/DetailContainer.js
@@ -1,7 +1,28 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import DetailPresenter from "./DetailPresenter";
 
-const DetailContainer = () => {
+const DetailContainer = (props) => {
+  const [result, setResult] = useState(null);
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    // parameter로 넘겨진 id값을 받고
+    const {
+      params: { id },
+    } = props.match;
+    const {
+      history: { push },
+    } = props;
+    console.log(props);
+    //숫자가 아닌 string형태로 넘겨진 id는 다시 홈으로 push해준다.
+    const parsedId = parseInt(id);
+    if (isNaN(parsedId)) {
+      //함수를 종료하기 위해서 return을 사용해 준다.
+      return push("/");
+    }
+  }, []);
+
   return <DetailPresenter />;
 };
 
