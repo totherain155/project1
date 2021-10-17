ff87edd098101c9196d32301b13ab905c4da71d5
totherain155 <totherain155@gmail.com>
2021-10-17 21:52:26 +0900

    modify TVPresenter

--- src/Routes/TV/TVPresenter.js
+++ src/Routes/TV/TVPresenter.js
@@ -1,10 +1,25 @@
 import React from "react";
 import styled from "styled-components";
 import PropTypes from "prop-types";
+import Section from "Components/Section";
 
-const TVPresenter = ({ topRated, popular, airingToday, loading, error }) => {
-  return <div>test</div>;
-};
+const Container = styled.div``;
+
+const TVPresenter = ({ topRated, popular, airingToday, loading, error }) =>
+  loading ? null : (
+    <Container>
+      {topRated && topRated.length > 0 && (
+        <Section title="Top Rated Shows">
+          {topRated.map((show) => show.name)}
+        </Section>
+      )}{" "}
+      {popular && popular.length > 0 && (
+        <Section title="Popular Shows">
+          {popular.map((show) => show.name)}
+        </Section>
+      )}{" "}
+    </Container>
+  );
 
 TVPresenter.propTypes = {
   topRated: PropTypes.array,
